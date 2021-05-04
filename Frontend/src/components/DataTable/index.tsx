36b810b3578189316b1a964348d223f6cd6edee1
Estêvão Filipe Cardoso da Silva <estevaof10@gmail.com>
2021-05-03 21:18:03 -0300

    Basic static components

--- Frontend/src/components/DataTable/index.tsx
+++ Frontend/src/components/DataTable/index.tsx
@@ -20,6 +20,86 @@ function DataTable() {
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
+
+                    <tr>
+                        <td>23/04/2021</td>
+                        <td>Super Man</td>
+                        <td>25</td>
+                        <td>30</td>
+                        <td>150017.00</td>
+                    </tr>
+
+                    <tr>
+                        <td>24/04/2021</td>
+                        <td>Batman</td>
+                        <td>37</td>
+                        <td>55</td>
+                        <td>160017.00</td>
+                    </tr>
+
+                    <tr>
+                        <td>25/04/2021</td>
+                        <td>Naruto</td>
+                        <td>33</td>
+                        <td>66</td>
+                        <td>16017.00</td>
+                    </tr>
+
+                    <tr>
+                        <td>26/04/2021</td>
+                        <td>Sasuke</td>
+                        <td>31</td>
+                        <td>fa-rotate-270</td>
+                        <td>13017.00</td>
+                    </tr>
+
+                    <tr>
+                        <td>27/04/2021</td>
+                        <td>Jack</td>
+                        <td>44</td>
+                        <td>18</td>
+                        <td>15017.00</td>
+                    </tr>
+
+                    <tr>
+                        <td>28/04/2021</td>
+                        <td>Julios</td>
+                        <td>10</td>
+                        <td>56</td>
+                        <td>190017.00</td>
+                    </tr>
+                    
+                    <tr>
+                        <td>29/04/2021</td>
+                        <td>Lanterna Verde</td>
+                        <td>39</td>
+                        <td>7</td>
+                        <td>10017.00</td>
+                    </tr>
+                    
+                    <tr>
+                        <td>30/04/2021</td>
+                        <td>Mulher Maravilha</td>
+                        <td>12</td>
+                        <td>12</td>
+                        <td>09017.00</td>
+                    </tr>
+                    
+                    <tr>
+                        <td>01/05/2021</td>
+                        <td>Apocalipse</td>
+                        <td>3</td>
+                        <td>11</td>
+                        <td>15017.00</td>
+                    </tr>
+                    
+                    <tr>
+                        <td>02/05/2021</td>
+                        <td>Thanos</td>
+                        <td>17</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
